330ccce110d45f8227f7c18bf4d8b85b690d0747
Hechmi idoudi <hechmiidoudi1@gmail.com>
2024-10-01 22:52:25 +0100

    commited at 10/01

--- Components/Sign_in/sign-in.js
+++ Components/Sign_in/sign-in.js
@@ -18,3 +18,9 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
 });
 
 
+// Ensure the DOM and all resources are fully loaded before hiding the loader
+window.addEventListener('load', function() {
+    console.log("Page fully loaded"); // To check if the event is firing correctly
+    // Add the 'loaded' class to the body to hide the loader
+    document.body.classList.add('loaded');
+  });
--- Components/Sign_up/signup.js
+++ Components/Sign_up/signup.js
@@ -21,3 +21,10 @@ document.getElementById('signupForm').addEventListener('submit', function (event
         alert('Please fill out all fields.');
     }
 });
+
+// Ensure the DOM and all resources are fully loaded before hiding the loader
+window.addEventListener('load', function() {
+    console.log("Page fully loaded"); // To check if the event is firing correctly
+    // Add the 'loaded' class to the body to hide the loader
+    document.body.classList.add('loaded');
+  });
